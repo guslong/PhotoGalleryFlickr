96ab78a9e8ce941a12648b0e644bcabbd139be51
guslong <guslong@GusAcer.fritz.box>
2014-03-30 19:59:07 +0200

    fixed

--- PhotoGallery/src/com/example/photogallery/PhotoGalleryActivity.java
+++ PhotoGallery/src/com/example/photogallery/PhotoGalleryActivity.java
@@ -1,8 +1,7 @@
 package com.example.photogallery;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class PhotoGalleryActivity extends ActionBarActivity {
+public class PhotoGalleryActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,32 +18,16 @@ public class PhotoGalleryActivity extends ActionBarActivity {
         setContentView(R.layout.activity_photo_gallery);
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
+            getFragmentManager().beginTransaction()
                     .add(R.id.container, new PhotoGalleryFragment())
                     .commit();
         }
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.photo_gallery, menu);
-        return true;
-    }
+   
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
 
    
 
--- PhotoGallery/src/com/example/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/example/photogallery/PhotoGalleryFragment.java
@@ -1,8 +1,8 @@
 package com.example.photogallery;
 
+import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
